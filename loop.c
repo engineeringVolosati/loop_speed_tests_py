75b7fc301d4647e13e0c2526e445164988876006
Andrey Kim <engineering.volosati@gmail.com>
2023-02-10 20:59:39 +0800

    some troubles in the loop.c uint64_t works like a int32_t

--- loop.c
+++ loop.c
@@ -1,12 +1,30 @@
-#include <stdint.h>
+// #include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 
-int loop(uint64_t numb)
+#define DEBUG 0
+
+#define NUMSYMBOLS 24
+#define BUFLEN (NUMSYMBOLS * 8)
+
+void debugprint(unsigned long long res, int go)
 {
-    uint64_t res = 0;
-    uint64_t i = 0;
-    for (i = 0; i < numb; i++)
+    if (DEBUG == 0 || go != 0)
+        return;
+    char buf[BUFLEN];
+    memset(buf, 0x00, BUFLEN);
+    sprintf(buf, "%d", res);
+    printf("The res is %s\n", buf);
+}
+
+int loop(unsigned long long numb)
+{
+    unsigned long long res = 0;
+    for (unsigned long int i = 0; i < numb; i++)
     {
         res += i;
+        debugprint(res, i % 1000000);
     }
+    debugprint(res, 0);
     return res;
 };
--- loop.py
+++ loop.py
@@ -1,30 +1,28 @@
 import random, ctypes
 import numpy as np
-from time import perf_counter
+from time import perf_counter, sleep
 
-numb = 10_000_000
-
-#==============================================================================
+numb = 5_000_000
+#=============================================================================
 # Сравним суммирование проходов по списку и использование функций sum и range
-#==============================================================================
-def cycle_example():
+#=============================================================================
+def cycle_example_fact():
     result = 0
     numbers = [num for num in range(numb)]
-
     for num in numbers:
         result += num
     print(result)
 
-def sum_example():
+def sum_example_fact():
     numbers = [num for num in range(numb)]
     print(sum(numbers))
 
-def sum_range():
+def sum_range_fact():
     print(sum(range(numb)))
 
-#==============================================================================
+#=============================================================================
 # Сравнение enumerate и доступа по индексам
-#==============================================================================
+#=============================================================================
 def enumerate_example():
     temp = 0
     numbers = [num for num in range(numb)]
@@ -41,9 +39,9 @@ def index_access():
         temp = numbers[num]
     print(temp)
 
-#==============================================================================
+#=============================================================================
 # Сравнение zip и обычного прохода циклами
-#==============================================================================
+#=============================================================================
 def idx_example():
     res = 0
     a = [num for num in range(numb)]
@@ -62,9 +60,9 @@ def zip_example():
         res = a_val + b_val
     print(res)
 
-#==============================================================================
+#=============================================================================
 # Сравнение циклов с генераторами
-#==============================================================================
+#=============================================================================
 USERS_BUY = [
     ("confirmed", 100),
     ("unconfirmed", 500),
@@ -94,40 +92,44 @@ def cycle_example():
             res += user[1]
     print(res)
 
-#==============================================================================
+#=============================================================================
 # Сравнение стандартных CPython функций и NumPy
-#==============================================================================
+#=============================================================================
 def sum_range():
     print(sum(range(numb)))
 
 def np_sum():
-    print(np.sum(np.arange(numb)))
+    print(np.sum(np.arange(numb).astype(dtype=np.uint64)))
 
-#==============================================================================
+#=============================================================================
 # Собственная реализация на чистом C
-#==============================================================================
+#=============================================================================
 testlib = ctypes.CDLL("./loop.dll")
 
 def loop_dll():
-    print(ctypes.c_uint32(testlib.loop(numb)))
+    print(testlib.loop(numb))
 
 
 if __name__ == "__main__":
-    print("Сравним суммирование проходов по списку и\nиспользование функций sum и range")
+    print("\x1b[36m"\
+        "Сравним суммирование проходов по списку и\n"\
+        "использование функций sum и range")
+
+    def wait():
+        input("\x1b[35m\nENTER to continue\n\x1b[36m")
+    start = perf_counter()
+    cycle_example_fact()
+    print(f"cycle_example_fact time: {(perf_counter() - start):.02f}")
 
     start = perf_counter()
-    cycle_example()
-    print(f"cycle_example time: {(perf_counter() - start):.02f}")
+    sum_example_fact()
+    print(f"sum_example_fact time: {(perf_counter() - start):.02f}")
 
     start = perf_counter()
-    sum_example()
-    print(f"sum_example time: {(perf_counter() - start):.02f}")
+    sum_range_fact()
+    print(f"sum_range_fact time: {(perf_counter() - start):.02f}")
 
-    start = perf_counter()
-    sum_range()
-    print(f"sum_range time: {(perf_counter() - start):.02f}")
-
-    input("\nENTER to continue\n")
+    wait()
 
     print("Сравнение enumerate и доступа по индексам")
 
@@ -139,7 +141,7 @@ if __name__ == "__main__":
     index_access()
     print(f"index_access time: {(perf_counter() - start):.02f}")
 
-    input("\nENTER to continue\n")
+    wait()
 
     print("Сравнение zip и обычного прохода циклами")
 
@@ -151,7 +153,7 @@ if __name__ == "__main__":
     zip_example()
     print(f"zip_example time: {(perf_counter() - start):.02f}")
 
-    input("\nENTER to continue\n")
+    wait()
 
     print("Сравнение циклов с генераторами")
 
@@ -169,14 +171,19 @@ if __name__ == "__main__":
     cycle_example()
     print(f"cycle_example time: {(perf_counter() - start):.02f}")
 
-    input("\nENTER to continue\n")
+    wait()
 
-    print("Сравнение стандартных CPython функций и NumPy")
+    print("Сравнение стандартных CPython функций, NumPy и С кода")
 
-    print('\nВАЖНО!\n numpy воспринимает int именно как int\n'\
-        'from -2,147,483,648 to 2,147,483,647 )\n'\
+    print('\n\x1b[3;93m\n'\
+        'ВАЖНО!\n numpy воспринимает int именно как int\n'\
+            '\033[4m'\
+        'from -2,147,483,648 to 2,147,483,647\n'\
+            '\033[0m\x1b[3;93m'\
         'поэтому для корректного сравнения результатов преобразовываем в\n\n'\
-        'NumPy - numb = np.uint64(numb)\nи\nC - numb = ctypes.c_uint64(numb)\n')
+        'NumPy base_64: numb = np.uint64(numb)\n'\
+            '\x1b[0m\n\x1b[36m')
+    sleep(3)
 
     start = perf_counter()
     sum_range()
@@ -184,18 +191,20 @@ if __name__ == "__main__":
 
     np_numb = np.uint64(numb)
     c_numb = ctypes.c_uint64(numb)
-    _ = numb
+    _numb = numb
 
     numb = np_numb
     start = perf_counter()
     np_sum()
     print(f"np_sum time: {(perf_counter() - start):.02f}")
 
-    numb = c_numb
+    numb = _numb
     start = perf_counter()
     loop_dll()
     print(f"loop_dll time: {(perf_counter() - start):.02f}")
-    print("И вот тут выскочил трабл с dll.\n"\
-        "Python воспринимает возвращаемое значение как int.")
-    print("Сейчас не важно, потом можно будет подсмотреть в\n"\
-        "исходниках CPython, если вдруг понадобится")
+    print("\x1b[3;93m\n"\
+        "И вот тут выскочил трабл с dll.\n"\
+        "В loop.c явно прописан uint64_t, но принимает отрицательные\n"\
+        "значения при оверлоаде как обычный int - int32_t..."\
+        "Сейчас не важно, потом можно будет подсмотреть"\
+            "\x1b[0m")
